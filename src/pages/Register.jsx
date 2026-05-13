24fa3e7ef293b932c35b031b48b9402ba4c95a3b
Pradeep M <pradeepmuthu1526@gmail.com>
2026-05-13 23:27:19 +0530

    pushed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,6 @@ import API from "../api/axios";
 import { useNavigate } from "react-router-dom";
 
 export default function Register() {
-
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
@@ -13,47 +12,37 @@ export default function Register() {
   });
 
   const handleChange = (e) => {
-
     setFormData({
       ...formData,
       [e.target.name]: e.target.value
     });
-
   };
 
   const handleSubmit = async (e) => {
-
     e.preventDefault();
 
     try {
-
       await API.post("/auth/register", formData);
 
       alert("Registration successful");
-
       navigate("/");
-
     } catch (err) {
+      console.log("FULL ERROR:", err.response?.data || err.message);
 
-      console.log(err);
-
-      alert("Registration failed");
-
+      alert(
+        err.response?.data?.message || "Registration failed"
+      );
     }
   };
 
   return (
-
     <div className="min-h-screen flex justify-center items-center bg-gradient-to-r from-blue-100 to-indigo-200">
-
       <div className="bg-white p-8 rounded-3xl shadow-2xl w-full max-w-md">
-
         <h1 className="text-3xl font-bold text-center text-indigo-700 mb-6">
           📝 Register
         </h1>
 
         <form onSubmit={handleSubmit} className="space-y-4">
-
           <input
             type="text"
             name="name"
@@ -81,11 +70,8 @@ export default function Register() {
           <button className="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-xl">
             Register
           </button>
-
         </form>
-
       </div>
-
     </div>
   );
 }
